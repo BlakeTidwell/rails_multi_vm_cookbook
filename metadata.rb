5a41e984c4624ec9407f1415108c15752087d29e
Blake Tidwell <batidwell@gmail.com>
2015-03-30 21:55:15 -0700

    Adds WIP on docs, specs, other practices-oriented stuff

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
 name             'rails_multi_vm'
 maintainer       'Blake Tidwell'
 maintainer_email 'YOUR_EMAIL'
-license          'Apache 2.0'
+license          'Apache 2 License'
 description      'Installs/Configures rails_multi_vm'
 long_description 'Installs/Configures rails_multi_vm'
 version          '0.1.0'
+
+depends          'apt', '~2.7.0'
+depends          'postgresql', '~3.3.4'
+depends          'database', '~2.1.8'
